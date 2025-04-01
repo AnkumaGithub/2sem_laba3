89fef451351902d42b4e74d1ffecb368073dd9d0
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2025-04-01 17:09:27 +0300

    shared_ptr

--- tasks/shared_ptr.cpp
+++ tasks/shared_ptr.cpp
@@ -1,6 +1,20 @@
 //
 // Created by Maksim Kuznetsov on 31.03.2025.
 //
+#include <iostream>
+#include <memory>
+
+class Data {
+public:
+    int value;
+    Data(int v) : value(v) { std::cout << "Data created" << std::endl; }
+    ~Data() { std::cout << "Data destroyed" << std::endl; }
+};
+
 int main() {
+    auto gg1 = std::make_shared<Data>(100);
+    std::cout << "Use count: " << gg1.use_count() << std::endl; // видим что только 1 владелец
+    auto gg2 = gg1;
+    std::cout << "Use count: " << gg1.use_count() << std::endl; // видим 2 владельца
     return 0;
 }
--- tasks/unique_ptr.cpp
+++ tasks/unique_ptr.cpp
@@ -8,7 +8,7 @@
 class File {
     std::unique_ptr<std::fstream> file;
 public:
-    explicit File(const std::string& filename) {
+        explicit File(const std::string& filename) {
         file = std::make_unique<std::fstream>(filename, std::ios::out);
         if (!file->is_open()) throw std::runtime_error("Cannot open file");
     }
@@ -18,7 +18,7 @@ public:
 
 int main() {
     for (int i = 0; i < 10; i += 1) {
-        auto gg = std::make_unique<int[]>(1000); // Автоматическое удаление
+        auto gg = std::make_unique<int>(1000); // Автоматическое удаление
     }
     // example
     File fh("test.txt");
