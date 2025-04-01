ca21c9f2296aab6f1c9ac1b9a2e7d5269f78c0ba
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2025-04-01 11:30:26 +0300

    memory_leak

--- tasks/memory_leak.cpp
+++ tasks/memory_leak.cpp
@@ -2,6 +2,11 @@
 // Created by Maksim Kuznetsov on 31.03.2025.
 //
 
+#include <iostream>
+
 int main() {
+  for (int i = 0; i < 100; i += 1) {
+    int* gg = new int[100]; // Утечка памяти, нет delete
+  }
   return 0;
 }
