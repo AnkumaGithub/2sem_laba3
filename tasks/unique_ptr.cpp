3bcc42ff1838ce16f88831b580fb774f20789dae
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2025-04-01 11:36:33 +0300

    unique_ptr

--- tasks/unique_ptr.cpp
+++ tasks/unique_ptr.cpp
@@ -1,6 +1,11 @@
 //
 // Created by Maksim Kuznetsov on 31.03.2025.
 //
+#include <iostream>
+
 int main() {
+    for (int i = 0; i < 10; ++i) {
+        auto gg = std::make_unique<int[]>(1000); // Автоматическое удаление
+    }
     return 0;
 }
